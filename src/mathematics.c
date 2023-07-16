ec57b579a6807545b1841404b1a8761aea8cf624
rodolfoamancio <rodolfojamancio@gmail.com>
2023-07-16 09:44:47 -0300

    methematics documentation

--- src/mathematics.c
+++ src/mathematics.c
@@ -1,3 +1,14 @@
+/* ***************************************************************************************************************
+ * This code was written as a part of LAMC, a program to perform molecular Monte Carlo simulations of linear
+ * alkanes on multiple ensembles.
+ * 
+ * This module mathematics.c is a code file for defining mathematical functions
+ * 
+ * Developer: Rodolfo J Amancio (rodolfojamancio@gmail.com)
+ * University of Campinas, School of Chemical Engineering, Campinas/SP -\sBrazil
+ * 
+ * **************************************************************************************************************/
+
 // ---------------------------------------------------------------------------------------------------------------
 // Headers loading
 // ---------------------------------------------------------------------------------------------------------------
