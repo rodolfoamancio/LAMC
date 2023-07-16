d26d26241a8cf10b392b0e86c04412b9052b7616
rodolfoamancio <rodolfojamancio@gmail.com>
2023-07-16 09:50:47 -0300

    fix header

--- src/molecule.c
+++ src/molecule.c
@@ -2,7 +2,7 @@
  * This code was written as a part of LAMC, a program to perform molecular Monte Carlo simulations of linear
  * alkanes on multiple ensembles.
  * 
- * This module mathematics.c is a code file for defining mathematical functions
+ * This module molecule.c is a code file for defining molecule function
  * 
  * Developer: Rodolfo J Amancio (rodolfojamancio@gmail.com)
  * University of Campinas, School of Chemical Engineering, Campinas/SP -\sBrazil
